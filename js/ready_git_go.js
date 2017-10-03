63bf770b783d97a21e48878e362a650d467d4ca6
Harsh <harsh@hkumar.me>
2017-10-02 17:38:19 -0700

    Add file structure, level identifier, and render function to state

--- js/ready_git_go.js
+++ js/ready_git_go.js
@@ -1,11 +1,18 @@
 import terminal from './terminal'
+import levelSelection from './level_selection'
+import render from './display'
 
 $(document).ready(() => {
   let gitState = {
     initialized: false,
     remote: false,
     currentCommand: '',
-    previousCommands: []
+    previousCommands: [],
+    fileStructure: {
+    },
+    level: 0,
+    render
   }
+  levelSelection(gitState)
   terminal(gitState);
 })
